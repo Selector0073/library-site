6bc7b2e1bc760d06038b3160f71cc237887700ed
Selector0073 <stanislavdh555@gmail.com>
2025-08-16 15:20:01 +0200

    save

--- LibrarySite/users/models.py
+++ LibrarySite/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    username = models.CharField(max_length=20)
+    email = models.EmailField()
+    passw = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.title
